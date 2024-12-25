4543acb57b9d8d1f73dad3a18d172faf71694060
redshiptsk <49164954+redshiptsk@users.noreply.github.com>
2024-12-25 19:09:11 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: `http://${process.env.DB_HOST}:${process.env.DB_PORT}`,
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
     credentials: true,
   });
