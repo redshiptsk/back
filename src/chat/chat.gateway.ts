eaf7bafba02df99b084738bd3f21fe51d8a7791a
Артём <fedor.artem.ivanov@mail.ru>
2025-01-14 15:38:00 +0700

    feat: add websocket

---
+++ src/chat/chat.gateway.ts
@@ -0,0 +1,29 @@
+import {
+    WebSocketGateway,
+    WebSocketServer,
+    SubscribeMessage,
+    MessageBody,
+    OnGatewayConnection,
+    OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({ cors: true })
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+    @WebSocketServer()
+    server: Server;
+
+    handleConnection(client: Socket) {
+        console.log(`Client connected: ${client.id}`);
+    }
+
+    handleDisconnect(client: Socket) {
+        console.log(`Client disconnected: ${client.id}`);
+    }
+
+    @SubscribeMessage('message')
+    handleMessage(@MessageBody() message: { sender: string; text: string }) {
+        console.log('Received message:', message);
+        this.server.emit('message', message);
+    }
+}
