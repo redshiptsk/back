5447f5347744ede7ec9d3e0afadd206a3cf86719
Ilya Shishkin <mostbook@mail.ru>
2025-01-17 01:24:39 +0700

    feat: moved client host to .env fix/backend/OZ-36

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5174',
+    origin: process.env.CLIENT_HOST ?? 'http://localhost:5174',
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
     credentials: true,
   });
