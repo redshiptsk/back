c2a76286956b7ee40c6c6213ace4b5a190b5d93f
Артём <fedor.artem.ivanov@mail.ru>
2025-02-07 20:22:50 +0700

    feat: middle result

--- src/chat/services/chat.service.ts
+++ src/chat/services/chat.service.ts
@@ -1,19 +1,19 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/sequelize';
+import {Injectable} from '@nestjs/common';
+import {InjectModel} from '@nestjs/sequelize';
 import {Message} from "../entities/message.model";
 
 @Injectable()
 export class ChatService {
     constructor(
         @InjectModel(Message)
-        private readonly messageModel: typeof Message,
+        private readonly messageModel: typeof Message
     ) {}
 
-    async saveMessage(data: { sender: string; text: string; imageUrl?: string }): Promise<Message> {
+    async saveMessage(data: { sender: string; text: string; imageUrl?: string; roomId: string }): Promise<Message> {
         return this.messageModel.create(data);
     }
 
-    async getAllMessages(): Promise<Message[]> {
-        return this.messageModel.findAll();
+    async getMessages(roomId: string): Promise<Message[]> {
+        return this.messageModel.findAll({where: {roomId}});
     }
 }
